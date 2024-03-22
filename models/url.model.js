70b6ffd34f54b6a9f35b3464ea56325cd8bfe236
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-22 13:15:30 +0530

    redis

--- models/url.model.js
+++ models/url.model.js
@@ -1,13 +1,14 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose")
 
 const urlSchema = mongoose.Schema({
-    shortURL :String,
-    originalURL :String
+    shortURL:String,
+    originalURL:String
 })
 
-const URLModel = mongoose.model("url",urlSchema);
+
+const URLModel = mongoose.model("url",urlSchema)
 
 
 module.exports={
-    URLModel,
+    URLModel
 }
